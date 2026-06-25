f690af0f2d968b66e84a6a9dfe667847864e9f7f
s1F10250215 <s1f102502159@iniad.org>
2026-06-25 11:42:46 +0900

    警告の修正3

--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django. utils. timezone import make_aware
 from django. utils. dateparse import parse_datetime
 from todo.models import Task
-
 # Create your views here.
 def index(request):
     if request.method == 'POST':
